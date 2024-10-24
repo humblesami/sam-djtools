54b53144dd37449e7e9654c0f16f36120145cb66
humblesami <samiakram@live.com>
2024-10-25 02:46:11 +0500

    sample app using imported methods from sam_tools

--- sample_usage/sample_app/admin.py
+++ sample_usage/sample_app/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from .models import Model1
 from sam_tools.dj import DjangoUtils
-from sam_tools.py import DateUtils
 
 
 class Model1Admin(admin.ModelAdmin):
     def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
-        print('\nsite url = '+ DjangoUtils.site_url(request))
-        print('Time = '+ DateUtils.now_str()+'\n')
-        return super().render_change_form(request, context, add=False, change=False, form_url='', obj=None)
+        context['site_url'] = DjangoUtils.site_url(request)
+        res = super().render_change_form(request, context, add, change, form_url)
+        return res
 
 
 # Register your models here.
